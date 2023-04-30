b19fdae5b2e7f9da1cca63a604839469650da855
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-04-30 09:02:21 +0200

    chore: install md icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,30 +1,31 @@
-import 'vuetify/styles'
-import { createVuetify, type ThemeDefinition } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify, type ThemeDefinition } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const SpeckleLCACustomTheme: ThemeDefinition = {
   dark: false,
   colors: {
-    primary: '#343634',
-    secondary: '#74706B',
-    accent: '#FCF25E',
-    error: '#FF5555',
-    warning: '#FF9100',
-    info: '#313BCF',
-    success: '#4caf50',
-    background: '#eeeeee',
-    text: '#FFFFFF'
+    primary: "#343634",
+    secondary: "#74706B",
+    accent: "#FCF25E",
+    error: "#FF5555",
+    warning: "#FF9100",
+    info: "#313BCF",
+    success: "#4caf50",
+    background: "#eeeeee",
+    text: "#FFFFFF"
   }
-}
+};
 
 export default createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: 'SpeckleLCACustomTheme',
+    defaultTheme: "SpeckleLCACustomTheme",
     themes: {
       SpeckleLCACustomTheme
     }
   }
-})
+});
